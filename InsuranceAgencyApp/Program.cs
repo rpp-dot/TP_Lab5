87f8d5ce0347ffde50b6fefcc703aea446a40a0c
rpp <rpp2182@gmail.com>
2024-11-11 20:58:06 +0300

    Добавлена регистрация клиента

--- InsuranceAgencyApp/Program.cs
+++ InsuranceAgencyApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InsuranceAgencyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в страховое агентство!");
+
+            RegisterClient();
+        }
+
+        static void RegisterClient()
+        {
+            Console.WriteLine("Введите имя клиента:");
+            string clientName = Console.ReadLine();
+            Console.WriteLine($"Клиент {clientName} зарегистрирован!");
+        }
+    }
+}
